bc04bf0207afe7f2250ead77c96d7e51a4db3fb8
lokyyyi <jyw9011@naver.com>
2023-09-14 15:16:46 +0900

    커밋 3 빼기 추가

--- Main.py
+++ Main.py
@@ -2,9 +2,17 @@ def add_func(n1, n2) :
     res = n1 + n2
     return res
 
+def sub_func(n1, n2) :
+    res = n1 - n2
+    return res
+
 num1 = 100
 num2 = 200
 
 result = add_func(num1, num2)
 
 print(num1, '+' , num2 , '=', result)
+
+result = sub_func(num1, num2)
+
+print(num1, '-' , num2 , '=', result)
